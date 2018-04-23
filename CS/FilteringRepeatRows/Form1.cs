48abbb7d967f0e9fee17c8b1df48c9302003515a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/FilteringRepeatRows/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FilteringRepeatRows
+{
+    public partial class Form1 : Form
+    {
+        List<string>[] filterFields = new List<string>[2];
+        FilterHelper helper;
+
+        public Form1()
+        {
+            InitializeComponent();
+            List<string> fields = new List<string>();
+            fields.Add("Name");
+            fields.Add("Company");
+            fields.Add("Address");
+            filterFields[0] = null;
+            filterFields[1] = fields;
+            helper = new FilterHelper(gridView1);
+            radioGroup1.SelectedIndex = 0;
+            gridControl1.DataSource = CreateTable(20);
+        }
+
+        DataTable CreateTable(int rowCount)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Name");
+            table.Columns.Add("Company");
+            table.Columns.Add("Age", typeof(int));
+            table.Columns.Add("Address");
+            int repeatCount = 3;
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < repeatCount; j++)
+                {
+                    table.Rows.Add(new object[] { String.Format("Name{0}", i), String.Format("Company{0}", i), 25 + i * repeatCount + j, String.Format("Address{0}", i) });
+                }
+            }
+            return table;
+        }
+
+        private void radioGroup1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            helper.Fields = filterFields[radioGroup1.SelectedIndex];
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to show only unique records in Grid
+
+
+<p>This example demonstrates how to show only unique values of specific fields in Grid. The main idea is to handle the CustomRowFilter event and set the e.Visible value based on the ContainsRow method value.</p>
+
+<br/>
+
+
